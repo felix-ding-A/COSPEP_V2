9c1de0d2d5398e0ed52e91c1e12cfd401018258c
felix-ding-A <40174900+felix-ding-A@users.noreply.github.com>
2026-02-18 21:19:51 +0800

    修改页面首页en 3

--- COSPEP_V2_Fronted/lighthouserc.js
+++ COSPEP_V2_Fronted/lighthouserc.js
@@ -3,6 +3,9 @@ module.exports = {
         collect: {
             url: ['https://cospep.com'],
             startServerCommand: undefined,
+            settings: {
+                chromeFlags: '--no-sandbox --headless=new --disable-gpu',
+            },
         },
         upload: {
             target: 'temporary-public-storage',
