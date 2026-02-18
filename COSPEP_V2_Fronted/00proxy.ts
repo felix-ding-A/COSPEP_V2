d52f386a4ae84015c34a68c30aa24fd70b3e431e
felix-ding-A <40174900+felix-ding-A@users.noreply.github.com>
2026-02-18 16:08:55 +0800

    tsconfig报错

--- COSPEP_V2_Fronted/00proxy.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './lib/navigation';
-
-export default createMiddleware(routing);
-
-export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(en|es|ru|ar)/:path*']
-};
