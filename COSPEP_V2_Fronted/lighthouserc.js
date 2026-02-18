582aa1c77ccca5c862cd8b1b748cb3e6b8ccd5b3
felix-ding-A <40174900+felix-ding-A@users.noreply.github.com>
2026-02-18 20:41:59 +0800

    添加lighthouserc

---
+++ COSPEP_V2_Fronted/lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ci: {
+        collect: {
+            url: ['https://cospep.com'],
+            startServerCommand: undefined,
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        },
+    },
+};
